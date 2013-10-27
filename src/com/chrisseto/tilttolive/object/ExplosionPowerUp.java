daeb27eb7e8643921fbd0693182ce70c781bbae9
chrisseto <chriskseto@gmail.com>
2013-10-27 13:52:53 -0400

    Progress

--- src/com/chrisseto/tilttolive/object/ExplosionPowerUp.java
+++ src/com/chrisseto/tilttolive/object/ExplosionPowerUp.java
@@ -1,18 +1,19 @@
 package com.chrisseto.tilttolive.object;
 
 import org.andengine.engine.camera.Camera;
+import org.andengine.entity.modifier.ScaleModifier;
 import org.andengine.opengl.texture.region.ITextureRegion;
 import org.andengine.opengl.vbo.VertexBufferObjectManager;
 
 import com.chrisseto.tilttolive.base.Ball;
 
-public class ExplosionPowerUp extends Ball{
+public class ExplosionPowerUp extends Ball {
 
 	public ExplosionPowerUp(float pX, float pY, int size,
 			ITextureRegion pTextureRegion,
 			VertexBufferObjectManager pVertexBufferObjectManager, Camera camera) {
 		super(pX, pY, size, pTextureRegion, pVertexBufferObjectManager, camera);
-		// TODO Auto-generated constructor stub
+		this.registerEntityModifier(new ScaleModifier(1, 1, 1));
 	}
 
 }
