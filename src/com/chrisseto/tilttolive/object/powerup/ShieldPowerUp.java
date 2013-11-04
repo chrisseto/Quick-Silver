ea5dff1aeb6a6d7ae552bc285394c31dff47d961
chrisseto <chriskseto@gmail.com>
2013-11-03 23:04:30 -0500

    new powerups

---
+++ src/com/chrisseto/tilttolive/object/powerup/ShieldPowerUp.java
@@ -0,0 +1,72 @@
+
+package com.chrisseto.tilttolive.object.powerup;
+
+import org.andengine.entity.IEntity;
+import org.andengine.entity.modifier.AlphaModifier;
+import org.andengine.entity.modifier.IEntityModifier.IEntityModifierListener;
+import org.andengine.opengl.texture.region.ITextureRegion;
+import org.andengine.opengl.vbo.VertexBufferObjectManager;
+import org.andengine.util.modifier.IModifier;
+
+import com.chrisseto.tilttolive.base.PowerUpBase;
+import com.chrisseto.tilttolive.managment.ActiveManager;
+import com.chrisseto.tilttolive.object.EnemyBall;
+
+public class ShieldPowerUp extends PowerUpBase {
+
+
+	public ShieldPowerUp(float pX, float pY,
+			ITextureRegion pTextureRegion, VertexBufferObjectManager vbom,
+			ActiveManager parent) {
+		super(parent.getPlayer().getX(), parent.getPlayer().getY(), 40, pTextureRegion, vbom, parent, 0);
+	}
+
+	@Override
+	public void update() {
+		this.setX(this.parent.getPlayer().getX());
+		this.setY(this.parent.getPlayer().getY());
+	}
+
+	@Override
+	protected void start() {
+		this.setAlpha(.7f);
+		
+	}
+
+	@Override
+	protected void beginFinish() {
+		this.registerEntityModifier(new AlphaModifier(1f,.7f,0,new IEntityModifierListener() {
+			
+			@Override
+			public void onModifierStarted(IModifier<IEntity> pModifier, IEntity pItem) {
+				// TODO Auto-generated method stub
+				
+			}
+			
+			@Override
+			public void onModifierFinished(IModifier<IEntity> pModifier, IEntity pItem) {
+				finish();
+				
+			}
+		}));
+		
+	}
+	
+	public float getRadius() {
+		return this.getWidth() / 2;
+	}
+	
+	@Override
+	public boolean collideswith(EnemyBall ball) {
+		if ((((this.getX() - ball.getX()) * (this.getX() - ball.getX())) + ((this
+				.getY() - ball.getY()) * (this.getY() - ball.getY())))
+				* (((this.getX() - ball.getX()) * (this.getX() - ball.getX())) + ((this
+						.getY() - ball.getY()) * (this.getY() - ball.getY()))) <= (getRadius() + ball
+				.getRadius()) * (getRadius() + ball.getRadius()))
+		{
+			beginFinish();
+			return true;
+		}
+		return false;
+	}
+}
---
+++ src/com/chrisseto/tilttolive/object/powerup/SpikePowerUp.java
@@ -0,0 +1,70 @@
+package com.chrisseto.tilttolive.object.powerup;
+
+import org.andengine.entity.IEntity;
+import org.andengine.entity.modifier.ScaleModifier;
+import org.andengine.entity.modifier.IEntityModifier.IEntityModifierListener;
+import org.andengine.opengl.texture.region.ITextureRegion;
+import org.andengine.opengl.vbo.VertexBufferObjectManager;
+import org.andengine.util.modifier.IModifier;
+
+import com.chrisseto.tilttolive.base.PowerUpBase;
+import com.chrisseto.tilttolive.managment.ActiveManager;
+import com.chrisseto.tilttolive.object.EnemyBall;
+
+public class SpikePowerUp extends PowerUpBase {
+
+	public SpikePowerUp(float pX, float pY, float size,
+			ITextureRegion pTextureRegion, VertexBufferObjectManager vbom,
+			ActiveManager parent) {
+		super(parent.getPlayer().getX(), parent.getPlayer().getY(), 45,
+				pTextureRegion, vbom, parent, 5);
+	}
+
+	@Override
+	public boolean collideswith(EnemyBall ball) {
+		return (((this.getX() - ball.getX()) * (this.getX() - ball.getX())) + ((this
+				.getY() - ball.getY()) * (this.getY() - ball.getY())))
+				* (((this.getX() - ball.getX()) * (this.getX() - ball.getX())) + ((this
+						.getY() - ball.getY()) * (this.getY() - ball.getY()))) <= (getRadius() + ball
+				.getRadius()) * (getRadius() + ball.getRadius());
+	}
+
+	public float getRadius() {
+		return this.getWidth() / 2;
+	}
+
+	@Override
+	protected void start() {
+		this.setScale(.1f);
+		this.registerEntityModifier(new ScaleModifier(.5f, .1f, 1));
+	}
+
+	@Override
+	protected void beginFinish() {
+		this.registerEntityModifier(new ScaleModifier(.5f, 1, .1f,
+				new IEntityModifierListener() {
+
+					@Override
+					public void onModifierStarted(IModifier<IEntity> pModifier,
+							IEntity pItem) {
+						// TODO Auto-generated method stub
+
+					}
+
+					@Override
+					public void onModifierFinished(
+							IModifier<IEntity> pModifier, IEntity pItem) {
+						finish();
+					}
+				}));
+
+	}
+
+	@Override
+	public void update() {
+		this.setX(this.parent.getPlayer().getX());
+		this.setY(this.parent.getPlayer().getY());
+
+	}
+
+}
